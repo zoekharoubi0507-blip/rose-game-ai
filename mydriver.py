5af11b1844d8896213ce53bec77adb199d33263c
bamba-debug <michaelbanchik1@gmail.com>
2026-07-13 10:30:04 +0300

    גרסא של אלישע

--- mydriver.py
+++ mydriver.py
@@ -10,9 +10,26 @@ driver_name = "MyDriver"
 def drive(world):
     x = world.car.x
     y = world.car.y
+    line=[]
+    for  i in range(0,3):
+        line[i]=world.get((i, y - 1))
+    possible=[]
+    for i in range(0, 3):
+        if line[i]==obstacles.NONE: possible[i]=0
+        elif line[i]==obstacles.CRACK: possible[i]=5
+        elif line[i]==obstacles.PENGUIN:possible[i]=10
+        elif line[i] == obstacles.WATER: possible[i] = 4
+        else : possible[i]=-10
+
+    if x==0:
+        max=possible[0]
+
     try:
-        obstacle = world.get((x, y - 1))
+        obstacle1 = world.get((x, y - 1))
     except IndexError:
-        else:
+
+
+    else:
         # Choose the best action for obstacle
     return actions.NONE
+
