70406274449fa1021f74103fa2dbead042ea1845
foxmyam-elisha <fox.m.yam@gmail.com>
2026-07-13 11:46:57 +0300

    ccc

--- mydriver.py
+++ mydriver.py
@@ -4,92 +4,62 @@ This driver does not do any action.
 
 from rose.common import obstacles, actions  # NOQA
 
-driver_name = "MyDriver_elisha"
+driver_name = "MyDriver"
 
 
 def drive(world):
     x = world.car.x
     y = world.car.y
-
-    # ====== סריקת שתי השורות קדימה ======
-    line1 = []  # שורה ראשונה (y - 1)
-    line2 = []  # שורה שנייה (y - 2)
-
+    line=[]
+    for  i in range(0,3):
+        line[i]=world.get((i, y - 1))
+    possible=[]
     for i in range(0, 3):
-        line1.append(world.get((i, y - 1)))
-        line2.append(world.get((i, y - 2)))
+        if line[i]==obstacles.NONE: possible[i]=0
+        elif line[i]==obstacles.CRACK: possible[i]=5
+        elif line[i]==obstacles.PENGUIN:possible[i]=10
+        elif line[i] == obstacles.WATER: possible[i] = 4
+        else : possible[i]=-10
 
-    # ====== חישוב ניקוד לכל משבצת ======
-    def get_score(obstacle):
-        if obstacle == obstacles.NONE:
-            return 0
-        elif obstacle == obstacles.CRACK:
-            return 5
-        elif obstacle == obstacles.PENGUIN:
-            return 10
-        elif obstacle == obstacles.WATER:
-            return 4
-        else:
-            return -10
-
-    # ניקודים לשורה הראשונה
-    scores1 = [get_score(line1[i]) for i in range(3)]
-
-    # ניקודים לשורה השנייה
-    scores2 = [get_score(line2[i]) for i in range(3)]
-
-    # ====== מציאת המסלול האופטימלי ======
-    # נחפש את הנתיב הטוב ביותר: עמודה בשורה 1 -> עמודה בשורה 2
-
-    best_score = float('-inf')
-    best_next_x = None
-    best_final_x = None
-
-    # קבע אילו עמודות אפשר להגיע אליהן מהעמודה הנוכחית
-    def can_reach(from_x, to_x):
-        return abs(from_x - to_x) <= 1
-
-    # בדוק כל נתיב אפשרי
-    for next_x in range(3):  # עמודה בשורה הראשונה
-        if not can_reach(x, next_x):
-            continue  # לא יכול להגיע לשם
-
-        for final_x in range(3):  # עמודה בשורה השנייה
-            if not can_reach(next_x, final_x):
-                continue  # לא יכול להגיע לשם
-
-            # חשב את הניקוד הכולל לנתיב זה
-            total_score = scores1[next_x] + scores2[final_x]
-
-            if total_score > best_score:
-                best_score = total_score
-                best_next_x = next_x
-                best_final_x = final_x
-
-    # ====== החלטה איזה פעולה לעשות ======
-    # בחר את הפעולה שתעביר אותנו לעמודה הטובה ביותר
-
-    if best_next_x is None:
-        best_next_x = x  # נשאר במקום אם אין אפשרות
-
-    if best_next_x < x:
-        move_action = actions.LEFT
-    elif best_next_x > x:
-        move_action = actions.RIGHT
+    if x==0:
+        maximum=max(possible[0],possible[1])
+    elif x==2:
+        maximum = max(possible[1], possible[2])
     else:
-        move_action = actions.NONE
+        maximum = max(possible[1], possible[2], possible[0])
 
-    # ====== טיפול בעצם המכשול ======
+    idxmax=possible.index(maximum)
+
+    if idxmax==0:
+        if x==1:
+            actions.LEFT
+        else:
+            actions.NONE
+    elif idxmax==1:
+        if x==1:
+            actions.NONE
+        elif x==0:
+            actions.RIGHT
+        else:
+            actions.LEFT
+    else:
+        if x==2:
+            actions.NONE
+        else:
+            actions.RIGHT
+
+
     try:
         obstacle = world.get((x, y - 1))
-
-        if obstacle == obstacles.PENGUIN:
+        if obstacle== obstacles.PENGUIN:
             return actions.PICKUP
-        elif obstacle == obstacles.CRACK:
+        elif obstacle==obstacles.CRACK:
             return actions.JUMP
-        elif obstacle == obstacles.WATER:
+        elif obstacle==obstacles.WATER:
             return actions.BRAKE
         else:
-            return move_action
+             return actions.NONE
     except IndexError:
-        return move_action
+        return actions.NONE
+    return actions.NONE
+
