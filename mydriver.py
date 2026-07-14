52ec0fd94b3c93ccfe96d93212864b2c496d7239
bamba-debug <michaelbanchik1@gmail.com>
2026-07-14 15:01:42 +0300

    גרסא עובדת

--- mydriver.py
+++ mydriver.py
@@ -1,70 +1,65 @@
-"""
-This driver does not do any action.
-"""
+from rose.common import obstacles, actions
 
-from rose.common import obstacles, actions  # NOQA
-
-driver_name = "CarDriver"
-
+driver_name = "Group_8"
 def drive(world):
-    score = 0
     x = world.car.x
     y = world.car.y
     try:
-        obstacle = world.get((x, y - 1))
+        front = world.get((x, y - 1))
     except IndexError:
-        return actions.NONE
-    else:
-        obstacle = world.get((x, y - 2))
+        front = obstacles.NONE
 
-    ## מסתכל שלוש צעדים קדימה ובודק
-    LOOK_AHEAD = 3
-    for i in range(1, LOOK_AHEAD + 1):  # לולאה של 3 צעדים
-        target_y = y - i
+    if front == obstacles.PENGUIN:
+        return actions.PICKUP
+
+    if front == obstacles.CRACK:
+        return actions.JUMP
+
+    if front == obstacles.WATER:
+        return actions.BRAKE
+    lane_scores = {}
+    for lane in range(3):
+        score = 0
+        blocked = False
+        for dist in range(1, 5):
+            try:
+                obj = world.get((lane, y - dist))
+            except IndexError:
+                continue
+            multiplier = 7 - dist
+            if obj == obstacles.PENGUIN:
+                score += 20 * multiplier
+            elif obj == obstacles.CRACK:
+                score += 15 * multiplier
+            elif obj == obstacles.WATER:
+                score += 14 * multiplier
+            elif obj == obstacles.NONE:
+                score += 10 * multiplier
+            else:
+                # obstacles
+                score -= 10 * multiplier
+                if dist == 1:
+                    blocked = True
+        if blocked:
+            score -= 200
+        lane_scores[lane] = score
+    best_lane = x
+    best_score = lane_scores[x]
+    for lane in range(3):
+        if lane_scores[lane] > best_score + 10:
+            best_lane = lane
+            best_score = lane_scores[lane]
+    if best_lane > x:
         try:
-            obstacle_ahead = world.get((x, target_y))
+            if world.get((x + 1, y - 1)) == obstacles.NONE:
+                return actions.RIGHT
         except IndexError:
-            return actions.NONE
-        if obstacle_ahead == obstacles.PENGUIN:
-            if i == 1:
-                score += 10
-                return actions.PICKUP
-            return actions.NONE
+            pass
+    elif best_lane < x:
 
-        elif obstacle_ahead == obstacles.CRACK:
-            if i == 1:
-                score += 5
-                return actions.JUMP
-            return actions.NONE
-
-        elif obstacle_ahead == obstacles.WATER:
-            if i == 1:
-                score += 4
-                return actions.BRAKE
-            return actions.NONE
-
-        elif obstacle == obstacles.NONE:
-            return actions.NONE
-
-        ###עקיפת מכשולים
-        elif obstacle_ahead != obstacles.NONE:
-            try:
-                right_lane = world.get((x + 1, y))
-                if right_lane == obstacles.NONE:
-                    return actions.RIGHT
-            except IndexError:
-                pass
-            try:
-                left_lane = world.get((x - 1, y))
-                if left_lane == obstacles.NONE:
-                    return actions.LEFT
-            except IndexError:
-                pass
-            return actions.NONE
-
-        else:
-            return actions.RIGHT if (x % 3) == 0 else actions.LEFT
-
+        try:
+            if world.get((x - 1, y - 1)) == obstacles.NONE:
+                return actions.LEFT
+        except IndexError:
+            pass
     return actions.NONE
-
-
